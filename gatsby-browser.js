2716a0289f2d1d1c9ac71531a73d976e279c2b87
Quentin Guenther <qguenther@outlook.com>
2020-10-11 17:37:06 -0700

    prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,6 @@
 // Highlighting for code blocks
+
+import './src/styles/prism.css';
 import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
